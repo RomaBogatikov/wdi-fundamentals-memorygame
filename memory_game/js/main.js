bb1c17216864ab1c37c0b5dd38d65192727a1803
Roman Bogatikov <ivaroman2000@gmail.com>
2019-01-31 18:15:20 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,6 +27,16 @@ var cards = [
 
 var cardsInPlay = [];
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match");
@@ -36,17 +46,19 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}	
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
